7e538eac7f283e707e7d58839b47b9d65ef5b51f
ocheal <oli.cheal@gmail.com>
2021-02-21 16:53:55 +0000

    Update github profile

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(name="website",
       version="0.0.1",
       description="Source code for personal portfolio website",
-      url='https://github.com/ocheal/website',
+      url='https://github.com/olicheal/website',
       author='Oliver Cheal',
       packages=find_packages())
--- website/freezer.py
+++ website/freezer.py
@@ -3,5 +3,5 @@ from flask_frozen import Freezer
 from . import app
 
 freezer = Freezer(app)
-app.config["FREEZER_DESTINATION"] = "../../ocheal.github.io"
+app.config["FREEZER_DESTINATION"] = "../../olicheal.github.io"
 app.config["FREEZER_DESTINATION_IGNORE"] = [".git*"]
